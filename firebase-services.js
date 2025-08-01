2a7579b0f57946ce9570e3da385f9fcc92762076
Aldeir <aldeir@gmail.com>
2025-08-01 13:30:54 -0300

    Create firebase-services.js

---
+++ firebase-services.js
@@ -0,0 +1,47 @@
+// Arquivo: firebase-services.js
+// Responsabilidade: Único ponto de importação para todas as funções do SDK do Firebase.
+// Resolve conflitos de nome e centraliza as dependências.
+
+// Auth
+export { 
+    onAuthStateChanged, 
+    createUserWithEmailAndPassword, 
+    updateProfile, 
+    sendEmailVerification, 
+    signInWithEmailAndPassword, 
+    GoogleAuthProvider, 
+    signInWithPopup, 
+    getAdditionalUserInfo 
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+
+// Firestore
+export { 
+    doc, 
+    setDoc, 
+    getDoc, 
+    serverTimestamp, 
+    deleteDoc, 
+    collection, 
+    query, 
+    where, 
+    onSnapshot, 
+    getDocs, 
+    updateDoc, 
+    writeBatch, 
+    FieldValue 
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+// Storage (com alias para 'ref')
+export { 
+    ref as storageRef, 
+    uploadBytes, 
+    getDownloadURL 
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-storage.js";
+
+// Realtime Database (com alias para 'ref')
+export { 
+    ref as databaseRef, 
+    set, 
+    onDisconnect, 
+    onValue 
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
